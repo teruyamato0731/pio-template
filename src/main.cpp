eadb3e19009cc2e8a954c38d352ca3db9b569131
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-08-12 19:04:53 +0900

    style: コードの構造を指定

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include <mbed.h>
 
-BufferedSerial pc{USBTX, USBRX, 115200};
+// Const variable
 
+// Function prototype
+
+// IO
+BufferedSerial pc{USBTX, USBRX, 115200};
 // CAN can{PA_11, PA_12, (int)1e6};
 // CAN can{PB_12, PB_13, (int)1e6};
 // CANMessage msg;
-
 Timer timer;
 
+// Struct definition
+
+// Global variable
+
+/// @brief The application entry point.
 int main() {
+  // put your setup code here, to run once:
   printf("\nsetup\n");
   timer.start();
   auto pre = timer.elapsed_time();
   while(1) {
+    // put your main code here, to run repeatedly:
     auto now = timer.elapsed_time();
     if(now - pre > 20ms) {
       printf("hoge\n");
@@ -20,3 +30,5 @@ int main() {
     }
   }
 }
+
+// Function definition
